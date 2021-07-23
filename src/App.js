af5c705f46b71b21624d238ae6097f6ced45367f
Chris <namegpark@naver.com>
2021-07-24 02:12:11 +0900

    Prop-types practice

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5
   },
   {
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9
   },
   {
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 3
   },
   {
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.9
   },
   {
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.4
   }
 ];
 
-function Food({name, picture}){
+function Food({name, picture, rating}){
   console.log(name);
   return (
     <div>
       <h1>{name}</h1>
-      <img src={picture} />
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
 
   )
 }
 
+Food.propTypes = {
+
+};
+
 function App() {
   return (
     <div className="App">
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image}/>
+        <Food name={dish.name} picture={dish.image} rating={dish.rating}/>
       ))}
     </div>
   );
