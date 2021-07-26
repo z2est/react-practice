1aa4778bd9b8f2a38056a3cf7624a5b622cd762e
Chris <namegpark@naver.com>
2021-07-26 18:24:40 +0900

    Class Component State practice

--- src/App.js
+++ src/App.js
@@ -2,63 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 3
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.9
-  },
-  {
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.4
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+
+  render(){
+    return (
+      <div>
+        <h1>Number {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
 
-function Food({name, picture, rating}){
-  console.log(name);
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
 
-  )
-}
 
-Food.propTypes = {
-
-};
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-    </div>
-  );
 }
 
 export default App;
